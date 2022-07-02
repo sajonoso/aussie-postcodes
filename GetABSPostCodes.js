9b5d7260b77be7049b517b8fa2cb05d5d3838fd2
Sajon Oso <sajonoso@users.noreply.github.com>
2022-07-02 19:33:59 +1000

    Update postcodes from 2021 data

--- GetABSPostCodes.js
+++ GetABSPostCodes.js
@@ -8,23 +8,23 @@
     This script generates a list of suburb name and post codes from the Australian Bureau of Statistics data
 
     The data files were downloaded from this link:
-    http://www.abs.gov.au/AUSSTATS/abs@.nsf/DetailsPage/1270.0.55.003July%202016?OpenDocument
+    https://www.abs.gov.au/statistics/standards/australian-statistical-geography-standard-asgs-edition-3/jul2021-jun2026/access-and-downloads/allocation-files
     
     The two required documents were obtained from the following 2 downloads:
     
-    NAME: Postal Areas ASGS Edition 2016 in .csv Format 
-    DOWNLOAD: http://www.abs.gov.au/ausstats/subscriber.nsf/log?openagent&1270055003_poa_2016_aust_csv.zip&1270.0.55.003&Data%20Cubes&BCC18002983CD965CA25802C00142BA4&0&July%202016&13.09.2016&Previous
-    DOCUMENT: POA_2016_AUST.csv
+    NAME: Postal Areas ASGS Edition 2021 in .csv Format 
+    DOWNLOAD: https://www.abs.gov.au/statistics/standards/australian-statistical-geography-standard-asgs-edition-3/jul2021-jun2026/access-and-downloads/allocation-files/POA_2021_AUST.xlsx
+    DOCUMENT: POA_2021_AUST.csv
     
-    NAME: State Suburbs ASGS Edition 2016 in .csv Format 
-    DOWNLOAD: http://www.abs.gov.au/ausstats/subscriber.nsf/log?openagent&1270055003_ssc_2016_aust_csv.zip&1270.0.55.003&Data%20Cubes&42CEBC5514202AFDCA25802C00142C05&0&July%202016&13.09.2016&Previous
-    DOCUMENT: SSC_2016_AUST.csv
+    NAME: Suburbs and Localities - 2021 ASGS Edition 2021 in .csv Format 
+    DOWNLOAD: https://www.abs.gov.au/statistics/standards/australian-statistical-geography-standard-asgs-edition-3/jul2021-jun2026/access-and-downloads/allocation-files/SAL_2021_AUST.xlsx
+    DOCUMENT: SAL_2021_AUST.csv
 */
 
 const fs = require('fs')
 
-const POA_DATA_FILE = 'POA_2016_AUST.csv'
-const SSC_DATA_FILE = 'SSC_2016_AUST.csv'
+const POA_DATA_FILE = 'POA_2021_AUST.csv'
+const SSC_DATA_FILE = 'SAL_2021_AUST.csv'
 const STATE_CODES = {
   1: 'NSW',
   2: 'VIC',
@@ -64,7 +64,7 @@ function forEachLine(filename, fn) {
 const postcode_table = {}
 
 function addLineToPostcodeTable(line) {
-  const fields = line.split(',')
+  const fields = line.split('\t')
   const mb_code = fields[0]
   const post_code = fields[1]
   postcode_table[`${mb_code}`] = `${post_code}`
@@ -91,7 +91,7 @@ function fixSuburbName(rawSuburbName) {
 }
 
 function addLineToPostcodeList(line) {
-  const fields = line.split(',')
+  const fields = line.split('\t')
   const mb_code = fields[0]
   var ssc_name = fields[2]
   var state_code = STATE_CODES[fields[3]]
